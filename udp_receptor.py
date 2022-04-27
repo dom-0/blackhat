ae29e94845802e51628ef8e66ea24ce1b0a16aae
Code Kraft <code.kraft@protonmail.me>
2022-04-27 19:55:09 +0200

    Added UDP receptor

---
+++ udp_receptor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+
+import socket
+
+target_host = "127.0.0.1"
+target_port = 9997
+
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+client.sendto(b"AAABBBCCCC", (target_host,target_port))
+
+data,addr = client.recvfrom(4096)
+print (data.decode())
+
+client.close()
