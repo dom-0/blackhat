4cde6d894eb42e6d5d02d481cdc8ae7d4692f72c
Code Kraft <code.kraft@protonmail.me>
2022-05-01 11:52:35 +0200

    Final version of netkraft

--- netkraft.py
+++ netkraft.py
@@ -6,20 +6,18 @@ import subprocess
 import argparse
 import socket
 import threading
+import os
 
 def execute(cmd):
     cmd = cmd.strip()
     if not cmd: # incase of a random enter or Ctrl+D
         return 
-
     op = subprocess.check_output(shlex.split(cmd), stderr=subprocess.STDOUT)
     return (op.decode())
 
 
-def handle_client(client_socket, args):
-
+def handle_client(client_socket, server_socket, args):
     ####### Upload Logic
-
     if args.upload:
 
         file_buffer = b""
@@ -32,28 +30,35 @@ def handle_client(client_socket, args):
                 message = f"Saved file {args.upload}"
                 client_socket.send(message.encode())
                 break
-
         with open(args.upload, "wb") as f:
             f.write(file_buffer)
     
     ####### Mini Shell / Command Shell logic
-
     elif args.command:
         cmd_buffer = b""
         while True:
             try:
-                client_socket.send(b"#> ")
+                client_socket.send(b"\nNETKRAFT #> ")
                 while "\n" not in cmd_buffer.decode():
                     cmd_buffer += client_socket.recv(64)
                 response = execute(cmd_buffer.decode())
                 if response:
                     client_socket.send(response.encode())
                 cmd_buffer = b""
-            except Exception as e:
-                print(f"server killed {e}")
-                sys.exit()
-                
+            except UnicodeDecodeError: # Weird! Client interrupts could not be parsed as interrupts by server. Byte Codes?
+                print(f"server killed due to client keyboard interrupt")
+                server_socket.close()
+                client_socket.close()
+                os.system('kill -9 $PPID') # somehow sys.exit() doesn't seem to do the trick
+
+            except FileNotFoundError as fnf: # Wrong command or filename
+                print(f"Seems like a wrong command: {fnf}")
+                client_socket.send(b"Error: Wrong command or filename\n")
+                cmd_buffer = b""
+
+    ####### Ad-hoc command execution 
         
+
 
 class NetCat:
     def __init__(self, args, buffer=""):
@@ -61,11 +66,10 @@ class NetCat:
         self.buffer = buffer
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+        # self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
 
     def run(self):
         if self.args.listen:
-            
             self.socket.bind(('0.0.0.0', self.args.port))
             self.socket.listen(5)
             print (f'[*] Listening at port {self.args.port} on all local IP addresses...')
@@ -75,12 +79,20 @@ class NetCat:
                 try:
                     client, _ = self.socket.accept()
                     print (f'[*] >>>> Accepted Connection from {client}')
-                    client_handler = threading.Thread(target=handle_client, args=(client, self.args))
-                    client_handler.start()
-                
-
+                    client_handler = threading.Thread(target=handle_client, args=(client, self.socket, self.args))
+                    client_handler.start()       
                 except KeyboardInterrupt:
                     sys.exit()
+
+        else:
+            self.send()
+            
+            
+    def send(self):
+        """Send data to the target host."""
+        self.socket.connect((self.args.target, self.args.port))
+        if self.buffer:
+            self.socket.send(self.buffer)
 
             
 
